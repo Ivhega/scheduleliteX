e1df5d76d06c060461842910772b0f0ae78a0fac
Adam <adam.devlaming@edu.sait.ca>
2024-08-06 18:48:54 -0600

    small update in config

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,6 @@
 //const BASE_URL = 'http://192.168.1.96:3000'; //Ivan
  const BASE_URL = 'http://192.168.1.77:3000'; //Adam
 
+//const BASE_URL = 'Enter your IP address here';
+
 export default BASE_URL;
